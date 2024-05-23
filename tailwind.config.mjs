6ffdc9ba527b1bdcd08c7f26222f67d02014d590
nilspineda <nilspineda@gmail.com>
2024-05-23 18:25:39 -0500

    Update
    Add tailwind, Add Alpinejs, add Navbar

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		
+		extend: {
+			colors: {
+				current: 'currentColor',
+				Verde: '#abcf3a',
+				Rosado: '#9d4685'
+
+			}
+	},
+	plugins: [],
+	
+	}
+}
